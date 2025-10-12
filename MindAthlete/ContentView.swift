38d3b27810d630f995659d55bc4d8def82c81f93
Renato <85751994+RESERIVRAM@users.noreply.github.com>
2025-10-11 23:58:49 -0500

    Initial Commit

---
+++ MindAthlete/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindAthlete
+//
+//  Created by Renato Riva on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindAthlete/MindAthleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindAthleteApp.swift
+//  MindAthlete
+//
+//  Created by Renato Riva on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MindAthleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
